a68be9291f0a5c88d8228b3fc94b5ff3ebe2535f
minkyu <tour00389@gmail.com>
2023-12-29 22:56:33 +0900

    refactor `[Wallet.js]` : Implement latest record selection for update and add method descriptions
    - Added comments to each method in `Wallet` class for better code readability.
    - Added the use of `findFirst` method to select the most recent record in the `updateData` method.
    - Used `orderBy` option to sort the records in descending order based on `expirationDate`, ensuring the most recent record is selected for update.

--- back/src/db/model/Wallet.js
+++ back/src/db/model/Wallet.js
@@ -1,6 +1,7 @@
 import { db } from "../db";
 
 class Wallet {
+  // 포인트 내역 조회
   static async findWalletByUserId(userId) {
     const wallet = db.wallet.findMany({
       where: {
@@ -13,6 +14,7 @@ class Wallet {
     return wallet;
   }
 
+  // 환전 내역 생성
   static async letChanges(data) {
     const wallet = db.wallet.create({
       data,
@@ -20,10 +22,18 @@ class Wallet {
     return wallet;
   }
 
+  // 최신 내역 Date 업데이트
   static async updateData(id) {
+    // 최신 내역 조회
+    const latestRecord = await db.wallet.findFirst({
+      where: { id },
+      orderBy: { expirationDate: "desc" },
+    });
+
+    // 최신 내역 수정
     const updatedWallet = db.wallet.update({
       where: {
-        id,
+        id: latestRecord.id,
       },
       data: {
         expirationDate: new Date(),
